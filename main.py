ed75f31079e832b9a339c33610a241928ad3f32a
Adriano Zaniboni De Pellegrin <adrianozdp@gmail.com>
2023-03-08 21:18:48 -0300

    Initial commit
    Signed-off-by: Adriano Zaniboni De Pellegrin <adrianozdp@gmail.com>

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+import praw
+import requests
+from urllib.parse import urljoin
+from datetime import datetime, timedelta
+
+from dotenv import load_dotenv
+
+SUBREDDIT = "tibiawars"
+
+whitelist_streamers = ["pantibiabot", "nattank", "nenazthegodz", "gota_cwb", 
+    "romaoz", "alemaotv", "renatimtv", "herrerodanica", "japakendy", "rubini", 
+    "terykzot", "fabio_headless", "retired_severo", "willianhumilde", "chobbyzera"]
+
+load_dotenv(override=False)
+
+REDDIT_CLIENT_ID = os.environ.get("REDDIT_CLIENT_ID", "")
+REDDIT_CLIENT_SECRET = os.environ.get("REDDIT_CLIENT_SECRET", "")
+REDDIT_REFRESH_TOKEN = os.environ.get("REDDIT_REFRESH_TOKEN", "")
+REDDIT_URI = os.environ.get("REDDIT_URI", "")
+TWITCH_AUTH = os.environ.get("TWITCH_AUTH", "")
+TWITCH_CLIENT_ID = os.environ.get("TWITCH_CLIENT_ID", "")
+
+class ClipNotFound(Exception):
+    pass
+
+def get_reddit():
+    return praw.Reddit(
+        client_id=REDDIT_CLIENT_ID,
+        client_secret=REDDIT_CLIENT_SECRET,
+        refresh_token=REDDIT_REFRESH_TOKEN,
+        user_agent="TibiaWars/1.0 by u/panzp",
+        redirect_uri=REDDIT_URI
+    )
+
+def publish_clip(reddit, title, url):
+    reddit.subreddit(SUBREDDIT).submit(title, url=url)
+
+
+def get_clips():
+    now = datetime.now()
+    one_hour_ago = now - timedelta(hours = 1)
+    url = urljoin(
+        "https://api.twitch.tv/",
+        f"helix/clips?game_id=19619&started_at={one_hour_ago.isoformat()}Z&ended_at={now.isoformat()}Z",
+    )
+    headers = {'Authorization': f'Bearer {TWITCH_AUTH}', 'Client-Id': TWITCH_CLIENT_ID}
+    clips = requests.request("GET", url, headers=headers, data={}).json()
+    filtered_clips = [
+        clip
+        for clip in clips["data"]
+        if clip["broadcaster_name"].lower() in whitelist_streamers
+        and clip["view_count"] > 10
+    ]
+    print(filtered_clips)
+    if not filtered_clips[0].get("title"):
+        raise ClipNotFound("A clip wasn't found.")
+
+    return filtered_clips[0]["title"], filtered_clips[0]["url"]
+
+
+def main():
+    title, url = get_clips()
+    reddit = get_reddit()
+    publish_clip(reddit, title, url)
+
+if __name__ == "__main__":
+    main()
